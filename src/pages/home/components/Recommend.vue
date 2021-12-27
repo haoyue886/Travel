b5774bbcb0abdf055f8baf97cf3aad84fcfb17ba
‘haoyue’ <1762351793@qq.com>
2021-12-27 17:08:19 +0800

    recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+        <li class="item border-bottom" 
+        v-for="item of recommendList" 
+        :key="item.id"
+        >
+            <img class="item-img" :src="item.imgUrl" >
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+                <button class="item-button">查看详情</button>
+            </div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+name:'HomeRecommend',
+data() {
+    return {
+        recommendList:[
+            {
+                id:'001',
+                imgUrl:'https://img0.baidu.com/it/u=4089622531,494058583&fm=26&fmt=auto',
+                title:'故宫博物院',
+                desc:'建立于1925年，是在明朝、清朝两代皇宫及其收藏的基础上建立起来的中国综合性博物馆'
+            },
+                {
+                id:'002',
+                imgUrl:'https://img0.baidu.com/it/u=4089622531,494058583&fm=26&fmt=auto',
+                title:'故宫博物院',
+                desc:'建立于1925年，是在明朝、清朝两代皇宫及其收藏的基础上建立起来的中国综合性博物馆'
+            },
+                {
+                id:'003',
+                imgUrl:'https://img0.baidu.com/it/u=4089622531,494058583&fm=26&fmt=auto',
+                title:'故宫博物院',
+                desc:'建立于1925年，是在明朝、清朝两代皇宫及其收藏的基础上建立起来的中国综合性博物馆'
+            }
+        ]
+    }
+},
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of List"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+     name: 'HomeWeekend',
+    data() {
+      return {
+            List:[
+            {
+                id:'001',
+                imgUrl:'https://img0.baidu.com/it/u=4089622531,494058583&fm=26&fmt=auto',
+                title:'故宫博物院',
+                desc:'建立于1925年，是在明朝、清朝两代皇宫及其收藏的基础上建立起来的中国综合性博物馆'
+            },
+                {
+                id:'002',
+                imgUrl:'https://img0.baidu.com/it/u=4089622531,494058583&fm=26&fmt=auto',
+                title:'故宫博物院',
+                desc:'建立于1925年，是在明朝、清朝两代皇宫及其收藏的基础上建立起来的中国综合性博物馆'
+            },
+                {
+                id:'003',
+                imgUrl:'https://img0.baidu.com/it/u=4089622531,494058583&fm=26&fmt=auto',
+                title:'故宫博物院',
+                desc:'建立于1925年，是在明朝、清朝两代皇宫及其收藏的基础上建立起来的中国综合性博物馆'
+            }
+        ]
+      }
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.09%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
