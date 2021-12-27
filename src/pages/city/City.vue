c5f954c2db56d2d394f65edadda358ab8aa5fb01
‘haoyue’ <1762351793@qq.com>
2021-12-27 22:02:45 +0800

    city search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
       <city-header></city-header>
+      <CitySearch></CitySearch>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
 export default {
 name:'City',
 components:{
  CityHeader,
+ CitySearch,
 }
 }
 </script>
