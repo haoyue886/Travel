4cf87ab077d24df3a29d315ac6bd6101fd0cd975
‘haoyue’ <1762351793@qq.com>
2022-02-20 17:03:29 +0800

    detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,23 +2,44 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="dd"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue'
 import DetailHeader from './components/Header.vue'
+import DetailList from './components/List'
 export default {
 name:'Detail',
+data() {
+  return {
+    list:[
+      {title:'学生票',
+      children:[
+        {title:'徐小夕',
+        children:[
+           {title:'发发发'}
+        ]
+        }
+      ]
+      },
+      {title:'成人票'},
+      {title:'单身贵族票'}
+    ]
+  }
+},
 components:{
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
 }
 }
 </script>
 
 <style lang="stylus" scoped>
-.dd
+.content
  height: 40rem
 </style>
